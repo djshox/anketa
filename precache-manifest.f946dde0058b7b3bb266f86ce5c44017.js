02e228eddcaebcc16036365163ddd8f72e8df94e
Dj shox <djshox1990@gmail.com>
2020-08-11 18:16:12 +0500

    Updates

---
+++ precache-manifest.f946dde0058b7b3bb266f86ce5c44017.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383107108925f6c1700f85f69b5b6a5c",
+    "url": "/anketa/index.html"
+  },
+  {
+    "revision": "9251e2d2521c5d9e8c39",
+    "url": "/anketa/static/css/4.565387ca.chunk.css"
+  },
+  {
+    "revision": "98d44298b8955526de7b",
+    "url": "/anketa/static/css/5.0485a127.chunk.css"
+  },
+  {
+    "revision": "6eb8c937f0264cf9488f",
+    "url": "/anketa/static/css/7.944bd02f.chunk.css"
+  },
+  {
+    "revision": "68ce04c798fc39a5bdb4",
+    "url": "/anketa/static/css/8.8b25fd53.chunk.css"
+  },
+  {
+    "revision": "f8b818d8394472bef6e2",
+    "url": "/anketa/static/css/main.f0c546ef.chunk.css"
+  },
+  {
+    "revision": "d878cad8ff6eb737ac92",
+    "url": "/anketa/static/js/0.19dcd2cf.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/anketa/static/js/0.19dcd2cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68cd45e97855a0bcabd0",
+    "url": "/anketa/static/js/10.9975bef1.chunk.js"
+  },
+  {
+    "revision": "6cb84fdb9f7004062f3e",
+    "url": "/anketa/static/js/11.be90afd5.chunk.js"
+  },
+  {
+    "revision": "1e6ace06c4f2e46083b4",
+    "url": "/anketa/static/js/12.c026806f.chunk.js"
+  },
+  {
+    "revision": "1ffca19787ed1154b431",
+    "url": "/anketa/static/js/13.20fa41f3.chunk.js"
+  },
+  {
+    "revision": "fe7f5032daef217e9c25",
+    "url": "/anketa/static/js/3.f56a6a5c.chunk.js"
+  },
+  {
+    "revision": "9251e2d2521c5d9e8c39",
+    "url": "/anketa/static/js/4.c51e7401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/anketa/static/js/4.c51e7401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d44298b8955526de7b",
+    "url": "/anketa/static/js/5.3f6c5635.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/anketa/static/js/5.3f6c5635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8397c83af58813f386",
+    "url": "/anketa/static/js/6.4c81b3b2.chunk.js"
+  },
+  {
+    "revision": "6eb8c937f0264cf9488f",
+    "url": "/anketa/static/js/7.374e14e1.chunk.js"
+  },
+  {
+    "revision": "68ce04c798fc39a5bdb4",
+    "url": "/anketa/static/js/8.8ccbd6aa.chunk.js"
+  },
+  {
+    "revision": "7dd31e08f678022a91e6",
+    "url": "/anketa/static/js/9.0b359929.chunk.js"
+  },
+  {
+    "revision": "f8b818d8394472bef6e2",
+    "url": "/anketa/static/js/main.a9ea8e16.chunk.js"
+  },
+  {
+    "revision": "5a16a75055ac62bcb486",
+    "url": "/anketa/static/js/runtime-main.33cf6c3c.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/anketa/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/anketa/static/media/element-icons.d2f69a92.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1f604543f6e0ba9d387fe6d4cee9b02.js"
+  "/anketa/precache-manifest.f946dde0058b7b3bb266f86ce5c44017.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anketa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
